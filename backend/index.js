02b275d524f0a513405abf8c3c34722db4d2d4ed
errorbegone <162623054+errorbegone@users.noreply.github.com>
2024-06-21 19:58:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose')
 const userRouter = require('./routes/userRoutes')
 const blogRouter = require('./routes/blogRoutes')
-// Password: DM5u5QB786Y1AkMj
+
 const app = express();
 const cors = require('cors')
 app.use(express.json());
 app.use(cors());
 app.use('/user', userRouter.router);
 app.use('/blogs', blogRouter.router);
-mongoose.connect('mongodb+srv://thakareas:18YPNwYhuGLose5F@cluster0.lkl0n3l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
+mongoose.connect('URL').then(() => {
     app.listen(5000, () => {
         console.log("APP Started");
     });
